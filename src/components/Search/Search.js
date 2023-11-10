b35eab4e8b4dd92c93fcbfb55f149516f7615568
Jen Nguyen <jenniferannnguyen@live.com>
2023-11-10 16:50:17 -0700

    Fix styling in some places

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
+import './Search.css'
 
 const Search = ({ setDrinks, drinks }) => {
   const [searchInput, setSearchInput] = useState("");
   const [error, setError] = useState("");
 
   useEffect(() => {
-    // Filter drinks whenever searchInput changes
-    const filteredDrinks = drinks.filter((drink) =>
-      drink.strDrink.toLowerCase().includes(searchInput.toLowerCase())
+    const filteredDrinks = drinks.filter(drink =>
+      drink.strDrink.toLowerCase().includes(searchInput.toLowerCase()),
     );
     setDrinks(filteredDrinks);
 
@@ -17,57 +17,58 @@ const Search = ({ setDrinks, drinks }) => {
       setError("");
     }
 
-    if(searchInput === ""){
-      setDrinks(drinks)
+    if (searchInput === "") {
+      setDrinks(drinks);
     }
   }, [searchInput, setSearchInput]);
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     setSearchInput(event.target.value);
   };
 
   const handleSearch = e => {
-        e.preventDefault();
-        {
-          const filtered = drinks.filter(drink =>
-            drink.strDrink.toLowerCase().includes(searchInput.toLowerCase()),
-          );
-          setDrinks(filtered);
-    
-          if (filtered.length === 0) {
-            setError("No drinks found.");
-          } else {
-            setError("");
-          }
-        }
-      };
+    e.preventDefault();
+    {
+      const filtered = drinks.filter(drink =>
+        drink.strDrink.toLowerCase().includes(searchInput.toLowerCase()),
+      );
+      setDrinks(filtered);
+
+      if (filtered.length === 0) {
+        setError("No drinks found.");
+      } else {
+        setError("");
+      }
+    }
+  };
 
   const setAllDrinks = () => {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
-    .then((resp) => {
-      if (resp.status === 404) {
-        throw new Error('Page not found');
-      } else if (!resp.ok) {
-        throw new Error('Oops! Something went wrong');
-      }
-      return resp.json();
-    })
-    .then((data) => {
-      setDrinks(data.drinks)
-    })
-    .catch((error) => {
-      if (error.name === 'TypeError') {
-        console.error('Oops! Something went wrong');
-      } else {
-        console.error(error);
-      }
-    });
-}
- 
-useEffect(() => {
-  setAllDrinks()
-}, [])
-  
+    fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic",
+    )
+      .then(resp => {
+        if (resp.status === 404) {
+          throw new Error("Page not found");
+        } else if (!resp.ok) {
+          throw new Error("Oops! Something went wrong");
+        }
+        return resp.json();
+      })
+      .then(data => {
+        setDrinks(data.drinks);
+      })
+      .catch(error => {
+        if (error.name === "TypeError") {
+          console.error("Oops! Something went wrong");
+        } else {
+          console.error(error);
+        }
+      });
+  };
+
+  useEffect(() => {
+    setAllDrinks();
+  }, []);
 
   return (
     <div>
@@ -79,69 +80,16 @@ useEffect(() => {
           value={searchInput}
           onChange={handleInputChange}
         />
-        <button
-          className="search-btn"
-          type="submit"
-        >
+        <button className="search-btn" type="submit">
           🔎
         </button>
         {error && <p>{error}</p>}
       </form>
-      <button className='back-to-all-drinks' onClick={setAllDrinks} >All Drinks</button>
+      <button className="all-drinks" onClick={setAllDrinks}>
+        All Drinks
+      </button>
     </div>
   );
 };
 
 export default Search;
-
-
-// const Search = ({ setDrinks, drinks }) => {
-//   const [searchInput, setSearchInput] = useState("");
-//   const [error, setError] = useState("");
-
-//   const handleSearch = e => {
-//     e.preventDefault();
-//     {
-//       const filtered = drinks.filter(drink =>
-//         drink.strDrink.toLowerCase().includes(searchInput.toLowerCase()),
-//       );
-//       setDrinks(filtered);
-
-//       if (filtered.length === 0) {
-//         setError("No drinks found.");
-//       } else {
-//         setError("");
-//       }
-//     }
-//   };
-
-//   const handleInputChange = event => {
-//     const value = event.target.value.toLowerCase();
-//     setSearchInput(value);
-//     console.log('SearchInput', searchInput)
-//     const filteredDrinks = drinks.filter(drink =>
-//       drink.strDrink.toLowerCase().includes(value),
-//     );
-
-//     if (value === '') {
-//       setDrinks(drinks);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSearch}>
-//       <input
-//         type="text"
-//         placeholder="Search for a drink"
-//         name="search"
-//         value={searchInput}
-//         onChange={handleInputChange}
-//       />
-//       <button className="search-btn" type="submit">
-//         🔎
-//       </button>
-//     </form>
-//   );
-// };
-
-// export default Search;
